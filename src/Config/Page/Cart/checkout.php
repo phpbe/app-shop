1959d0f210784c1ace56288646afe0dab7f52648
Lou Barnes <8981024@qq.com>
2023-03-13 14:11:41 +0800

    页面配置项

--- src/Config/Page/Cart/checkout.php
+++ src/Config/Page/Cart/checkout.php
@@ -2,6 +2,9 @@
 
 namespace Be\App\Shop\Config\Page\Cart;
 
+/**
+ * @BeConfig("结算")
+ */
 class checkout
 {
 
@@ -18,5 +21,36 @@ class checkout
         ],
     ];
 
+    /**
+     * @BeConfigItem("HEAD头标题",
+     *     description="HEAD头标题，用于SEO",
+     *     driver = "FormItemInput"
+     * )
+     */
+    public string $title = 'Checkout';
+
+    /**
+     * @BeConfigItem("Meta描述",
+     *     description="填写页面内容的简单描述，用于SEO",
+     *     driver = "FormItemInputTextArea"
+     * )
+     */
+    public string $metaDescription = '';
+
+    /**
+     * @BeConfigItem("Meta关键词",
+     *     description="填写页面内容的关键词，用于SEO",
+     *     driver = "FormItemInput"
+     * )
+     */
+    public string $metaKeywords = '';
+
+    /**
+     * @BeConfigItem("页面标题",
+     *     description="展示在页面内容中的标题，一般与HEAD头标题一致，两者相同时可不填写此项",
+     *     driver = "FormItemInput"
+     * )
+     */
+    public string $pageTitle = '';
 
 }
--- src/Config/Page/User/login.php
+++ src/Config/Page/User/login.php
@@ -2,9 +2,12 @@
 
 namespace Be\App\Shop\Config\Page\User;
 
+
+/**
+ * @BeConfig("登录")
+ */
 class login
 {
-
 
     public int $west = 0;
     public int $center = 100;
@@ -15,5 +18,39 @@ class login
             'name' => 'Theme.System.PageContent',
         ],
     ];
+
+
+    /**
+     * @BeConfigItem("HEAD头标题",
+     *     description="HEAD头标题，用于SEO",
+     *     driver = "FormItemInput"
+     * )
+     */
+    public string $title = 'Login';
+
+    /**
+     * @BeConfigItem("Meta描述",
+     *     description="填写页面内容的简单描述，用于SEO",
+     *     driver = "FormItemInputTextArea"
+     * )
+     */
+    public string $metaDescription = '';
+
+    /**
+     * @BeConfigItem("Meta关键词",
+     *     description="填写页面内容的关键词，用于SEO",
+     *     driver = "FormItemInput"
+     * )
+     */
+    public string $metaKeywords = '';
+
+    /**
+     * @BeConfigItem("页面标题",
+     *     description="展示在页面内容中的标题，一般与HEAD头标题一致，两者相同时可不填写此项",
+     *     driver = "FormItemInput"
+     * )
+     */
+    public string $pageTitle = '';
+
 }
 
