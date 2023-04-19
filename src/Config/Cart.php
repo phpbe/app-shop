58abfc516f7144eba1132df16aed40270e5749e3
Lou Barnes <8981024@qq.com>
2023-04-19 18:04:36 +0800

    购徨车 默认缓存 30 天

--- src/Config/Cart.php
+++ src/Config/Cart.php
@@ -10,7 +10,7 @@ class Cart
     /**
      * @BeConfigItem("缓存失效时间（天）", description="购物车优先使用缓存（如Redis）存放，缓存不存在时从数据库中加载载到缓存", driver="FormItemInputNumberInt")
      */
-    public int $cacheExpireDays = 3;
+    public int $cacheExpireDays = 30;
 
     /**
      * @BeConfigItem("最大条数", driver="FormItemInputNumberInt")
