3778050fee3cc2fec495856df140023633fe4c7a
Lou Barnes <8981024@qq.com>
2023-05-27 10:00:13 +0800

    默认 伪琢态网址

--- src/Config/Category.php
+++ src/Config/Category.php
@@ -10,7 +10,7 @@ class Category
     /**
      * @BeConfigItem("网址前缀", driver="FormItemInput", description="以 / 开头，谨慎改动。")
      */
-    public string $urlPrefix = '/collections/';
+    public string $urlPrefix = '/category/';
 
     /**
      * @BeConfigItem("网址后缀（如.html）", driver="FormItemInput")
--- src/Config/Product.php
+++ src/Config/Product.php
@@ -10,7 +10,7 @@ class Product
     /**
      * @BeConfigItem("网址前缀", driver="FormItemInput", description="以 / 开头，谨慎改动。")
      */
-    public string $urlPrefix = '/products/';
+    public string $urlPrefix = '/product/';
 
     /**
      * @BeConfigItem("网址后缀（如.html）", driver="FormItemInput")
